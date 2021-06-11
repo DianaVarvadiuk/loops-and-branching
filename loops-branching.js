3e7bb17ebc4f8586c3b2ccef86ec829a38907371
VarvadiukCursor <dianavarvadiuk@gmail.com>
2021-06-11 23:59:51 +0300

    fix  some loops and branching

--- loops-branching.js
+++ loops-branching.js
@@ -1,31 +1,31 @@
-let firstNumber = Number( prompt("Vvedit firstNumber  ", 10) );
-let secondNumber = Number( prompt("Vvedit secondNumber ", 100) );
+let firstNumber = +( prompt("Vvedit firstNumber  ", 10) );
+let secondNumber = +( prompt("Vvedit secondNumber ", 100) );
 
-while( isNaN(firstNumber) ){
+while(isNaN(firstNumber) ){
 
-    firstNumber = Number( prompt("Enter an integer") );
+    firstNumber = +( prompt("Enter an integer") );
 }
 console.log("firstNumber : ",firstNumber);
 console.log("Integer firstNumber ", !Number.isInteger(firstNumber) );
 
 while( isNaN(secondNumber) ) {
-    secondNumber = Number( prompt("Enter an integer") );
+    secondNumber = +( prompt("Enter an integer") );
 }
 console.log("secondNumber",secondNumber);
 console.log( "Integer secondNumber",!Number.isInteger(secondNumber) );
 
-const check = confirm("Skip even numbers?");
+const skipEven = confirm("Skip even numbers?");
 let result = 0;
 
-for( let i = firstNumber; i <= secondNumber; i++ ) {
+for(let i = firstNumber; i <= secondNumber; i++) {
 
- if (check) {
-    if ( i % 2 === 0) {
-    continue;
-    console.log("even number")
+
+    if (!skipEven && i % 2 === 0) {
+        continue;
+        console.log("even number")
     }
-    } else {
-    console.log("not an even number")
+     else {
+        console.log("not an even number")
     }
 result += i;
 }
